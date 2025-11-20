9426f3211bf25c3ba019cabb738a9ef4db4355a4
sachin panchal <sachinpanchal9942@gmail.com>
2025-11-20 16:59:35 +0530

    Learn about nested dynamic routes

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -1,4 +1,13 @@
-export default function docs({params} : {params : Promise<{slug : string[]}>}) {
+export default async function docs({params} : {params : Promise<{slug : string[]}>}) {
+    const {slug} = await params;
+    if(slug?.length == 2){
+        return (<>
+            <h1>Viewing docs for feature "{slug[0]}" and concept "{slug[1]}"</h1>
+        </>)
+    }
+    else if(slug?.length == 1){
+        <h1>Viewing docs for feature "{slug[0]}"</h1>
+    }
     return (
         <div>
             <h1>This is docs file</h1>
